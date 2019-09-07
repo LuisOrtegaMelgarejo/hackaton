f359aa19e433436716025e39bac733c7be8dde22
Jenny Vega <jelusita11@gmail.com>
2019-09-06 22:14:46 -0500

    pedidos screen

--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -4,6 +4,7 @@ import { createStackNavigator, createBottomTabNavigator } from 'react-navigation
 
 import TabBarIcon from '../components/TabBarIcon';
 import HomeScreen from '../screens/HomeScreen';
+import PedidosScreen from '../screens/PedidosScreen';
 
 const HomeStack = createStackNavigator({
   Home: HomeScreen
@@ -23,6 +24,25 @@ HomeStack.navigationOptions = {
   ),
 };
 
+const PedidosStack = createStackNavigator({
+  Home: PedidosScreen
+});
+
+PedidosStack.navigationOptions = {
+  tabBarLabel: 'Principal',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={
+        Platform.OS === 'ios'
+          ? `ios-information-circle${focused ? '' : '-outline'}`
+          : 'md-information-circle'
+      }
+    />
+  ),
+};
+
 export default createBottomTabNavigator({
-  HomeStack
+  HomeStack,
+  PedidosStack
 });
